ea7c4021b101ab937182b459bcd0465a028cd544
Eli <46053382+elilooksforwork@users.noreply.github.com>
2020-06-03 12:05:40 -0400

    Add files via upload

--- TestValidatePost.php
+++ TestValidatePost.php
@@ -1,8 +1,9 @@
 <?php
-    require_once('classes/global.php');
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
     $data = json_decode(file_get_contents('php://input'), true);
     //print_r($data);
-    //echo $data["operacion"];
     foreach ($data as $key => $value) {
         echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."\r\n";
     }
